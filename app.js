d8a088dac4d1103ee08ab2933502e0adb32850ce
Vahe Isaganyan <vahe.isag@outlook.com>
2022-10-18 19:09:03 -0700

    added highest number prompt and guess attempts.

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 let maximum = parseInt(prompt("Enter the highest number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:");
+    } else {
+        guess = prompt("Too low! Enter a new guess:");
+    }
+
+}
+
+if (guess === 'q') {
+    console.log("Quitting")
+} else {
+    console.log("You won!")
+    console.log(`You won! It took you ${attempts} guess/guesses`)
+}
